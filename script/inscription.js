b3bd2abc72c16939f19a9095bbd4664ce0bb6f2c
FDonovan12 <donovanferreira12@gmail.com>
2024-10-24 15:28:26 +0200

    prettier

--- script/inscription.js
+++ script/inscription.js
@@ -1,24 +1,33 @@
-const firstName = document.getElementById("first").value;
-const lastName = document.getElementById("last").value;
-const email = document.getElementById("email").value;
-const birthDate = document.getElementById("birthdate").value;
-const eventAttended = document.getElementById("quantity").value;
-const eventCity = document.querySelectorAll('[name="location"]').filter((location) => location.checked)
-const cityName= eventCity.empty ? "" : eventCity.value;
-const cgv = document.getElementById("checkbox1").value;
-const newsletter = document.getElementById("checkbox2").value;
+const firstName = document.getElementById('first').value;
+const lastName = document.getElementById('last').value;
+const email = document.getElementById('email').value;
+const birthDate = document.getElementById('birthdate').value;
+const eventAttended = document.getElementById('quantity').value;
+const eventCity = document
+    .querySelectorAll('[name="location"]')
+    .filter((location) => location.checked);
+const cityName = eventCity.empty ? '' : eventCity.value;
+const cgv = document.getElementById('checkbox1').value;
+const newsletter = document.getElementById('checkbox2').value;
 
 const socket = io();
 
-const formIsValid = firstName&&lastName&&email&&birthDate&&cityName&&cgv&&newsletter;
+const formIsValid = firstName && lastName && email && birthDate && cityName && cgv && newsletter;
 
-document.getElementById('form-inscription').addEventListener('submit', function(event) {
-
-    if (formIsValid){
-        alert("its all ok");
-        socket.emit("newUser", {firstName, lastName, email, birthDate, cityName, cgv, newsletter});
-    } else{
-        alert("fields required");
+document.getElementById('form-inscription').addEventListener('submit', function (event) {
+    if (formIsValid) {
+        alert('its all ok');
+        socket.emit('newUser', {
+            firstName,
+            lastName,
+            email,
+            birthDate,
+            cityName,
+            cgv,
+            newsletter,
+        });
+    } else {
+        alert('fields required');
         event.prenventDefault();
     }
 });
