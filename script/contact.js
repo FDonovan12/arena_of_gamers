5b5bd9421546d4d77771a68c76c05c16f1239ed9
fbanry <fabian.banry@protonmail.com>
2024-10-25 12:04:57 +0200

    Added validation to email, fixed button

--- script/contact.js
+++ script/contact.js
@@ -6,12 +6,17 @@ const formContent = document.getElementById('formContent');
 
 contactForm.addEventListener("submit", event => {
     event.preventDefault();
+    const regexEmail = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
     const email = formMail.value;
     const subject = formSubject.value;
     const content = formContent.value;
     if (email && subject && content) {
-        socket.emit("newContact", { email, subject, content });
-        alert("Votre message a été envoyé, merci !");
+        if (regexEmail.test(email)) {
+            socket.emit("newContact", { email, subject, content });
+            alert("Votre message a été envoyé, merci !");
+        } else {
+            alert("Veuillez renseigner une adresse mail valide");
+        }
     } else {
         alert("Veuillez renseigner tous les champs");
     }
