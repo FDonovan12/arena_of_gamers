8790daf9aa510320335d02a27c0bac9d4d43cdce
David De Sa <david.desa707@gmail.com>
2024-10-25 12:17:20 +0200

    c'est fini

--- script/pastEvent.js
+++ script/pastEvent.js
@@ -1,20 +1,17 @@
 const socket = io();
-const eventBox = document.getElementsByClassName('active');
 
 socket.on("pastEvent", (data) => {
-        afficherPastEvents(data);
+    printPastEvent(data);
 });
 
-function afficherPastEvents(pastEvent) {
+function printPastEvent(pastEvent) {
 
     const main = document.querySelector('#pastEvent');
     const mainDiv = document.createElement('div');
-
-    mainDiv = document.createElement('div')
     mainDiv.innerHTML= `
     <div>${pastEvent.name}</div>
     <div>${pastEvent.city}</div>
     <div>${pastEvent.eventDate}</div>
     `;
-    document.createElement('div')
+    main.appendChild(mainDiv);
 }
